9eb8be8d0c18546c7ac6212893f85550b0933c76
shimizu <shimizu>
2015-07-09 06:33:07 +0000

    default dev EMAIL_DOMAIN => use rctest.bmy.jp

--- app/config/require.dev.php
+++ app/config/require.dev.php
@@ -7,12 +7,13 @@ define('MAIN_PATH',     'http://wsb.sipss.jp/mobile_station_beauty/serverside/')
 //-- メールサーバー --//
 define('MAILSERVER_PORT', '25');
 //define('MAILSERVER_ADDRESS', '192.168.88.105');     // orig settings
-define('MAILSERVER_ADDRESS', 'mail.bmy.jp');       // test04.mobilestation.jp
+//define('MAILSERVER_ADDRESS', 'mail.bmy.jp');       // test04.mobilestation.jp
+define('MAILSERVER_ADDRESS', '221.186.135.77');       // test04.mobilestation.jp
 //define('MAILSERVER_ADDRESS', '218.216.75.20');      // stmp only
 
 define('MOBASUTE_PATH_LOCAL', '/var/www/html/');
 define('MOBASUTE_PATH', 'http://www.bmy.jp/');
 
-define('EMAIL_DOMAIN', 'bmy.jp');
+define('EMAIL_DOMAIN', 'rctest.bmy.jp');
 
 ?>
