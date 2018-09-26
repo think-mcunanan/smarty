8b7fa384cd6cdea536b874f1d3be1f1b203e7003
think-seron <marvin@think-ahead.jp>
2018-09-26 09:35:09 +0000

    update config file rc and online

--- app/config/require.online.php
+++ app/config/require.online.php
@@ -10,7 +10,7 @@ define('MAILSERVER_PORT', '25');
 define('MAILSERVER_ADDRESS', 'mail.bmy.jp');       // test04.mobilestation.jp
 //define('MAILSERVER_ADDRESS', '218.216.75.20');      // stmp only
 
-define('MOBASUTE_PATH_LOCAL', '/var/www2/html/');
+define('MOBASUTE_PATH_LOCAL', '/var/www2/htmlbeauty/');
 define('MOBASUTE_PATH', 'http://www.bmy.jp/');
 
 define('EMAIL_DOMAIN', 'bmy.jp');
@@ -25,3 +25,5 @@ define('KANZASHI_PATH', 'https://kanzashiapi.sipss.jp');
 //-- Webプロキシ --//
 putenv("http_proxy=http://192.168.88.13:10080");
 putenv("https_proxy=http://192.168.88.13:10080");
+
+?>
