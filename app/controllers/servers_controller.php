e75ab3d0bbd59e8c2a23ef265b6741ea5b555c74
Jagjit Singh <jagjit@think-ahead.jp>
2020-09-10 18:46:30 +0900

    Revert "fix for -  if salon id 0 exist"
    This reverts commit 7f0da1f404c95e61b4112e0415bd7b8344e7c0f2.

--- app/controllers/servers_controller.php
+++ app/controllers/servers_controller.php
@@ -1302,7 +1302,8 @@ class ServersController extends WebServicesController
             'SEX'              => 'xsd:int',
             'YOYAKU_DISPLAY'   => 'xsd:int',
             'WEB_DISPLAY'      => 'xsd:int',
-            'KANZASHI_SALON_POS_ID' => 'xsd:string',
+            'KANZASHI_ENABLED' => 'xsd:boolean',
+            'KANZASHI_SALON_POS_ID' => 'xsd:int',
             'ROWS'             => 'xsd:int',
             'PHONEROWS'        => 'xsd:int',
             'DISPLAY_ORDER'    => 'xsd:int',
@@ -4997,7 +4998,7 @@ class ServersController extends WebServicesController
             "storecode" => $storeinfo['storecode'],
             "staffcode" => $param['STAFFCODE'],
             "web_display" => $param['WEB_DISPLAY'],
-            "kanzashi_salon_pos_id" => empty($param['KANZASHI_SALON_POS_ID']) ? null : intval($param['KANZASHI_SALON_POS_ID']),
+            "kanzashi_salon_pos_id" => $param['KANZASHI_ENABLED'] === true ? $param['KANZASHI_SALON_POS_ID'] : null,
             "yoyaku_display" => $param['YOYAKU_DISPLAY'],
             "display_order" => $param['DISPLAY_ORDER'] === '' ? 'NULL' : $param['DISPLAY_ORDER']
         );
