75d380636525dba6f828949dcb68a9b20b59fc2e
think-mcunanan <mcunanan@think-ahead.jp>
2021-10-08 10:57:07 +0900

    Redmine-#5313:[レスポンス改善] サーバーサイドをPHP5→PHP7にアップグレードする - remove diagnostic log in wsGetReservationCounter function

--- app/controllers/servers_controller.php
+++ app/controllers/servers_controller.php
@@ -2363,8 +2363,7 @@ class ServersController extends WebServicesController
         'storeReservationCounter'   => array('struct' => array(
             'wrkr' => 'xsd:int',
             'bmr' => 'xsd:int',
-            'kanzashi_undecided' => 'xsd:int',
-            'log' => 'xsd:string',
+            'kanzashi_undecided' => 'xsd:int'
         )),
         '_storeReservationCounter'  => array(
             'array' => 'storeReservationCounter'
@@ -2955,26 +2954,16 @@ class ServersController extends WebServicesController
      */
     function wsGetReservationCounter($sessionid, $strcode, $datefr, $dateto)
     {
-        $to_miliseconds = function($time) {
-            return round($time * 1000, 3) . ' miliseconds';
-        };
         //===================================================================================
         //(Verify Session and Get DB name)
         //-----------------------------------------------------------------------------------
-        $start_time = microtime(true);
         $storeinfo = $this->YoyakuSession->Check($this);
         if ($storeinfo == false) {
             $this->_soap_server->fault(1, '', INVALID_SESSION);
             return;
         }
-        $end_time = microtime(true);
-        $log[] = "check_session_duration:". $to_miliseconds($end_time - $start_time);
 
-        $start_time = microtime(true);
         $this->Customer->set_company_database($storeinfo['dbname'], $this->Customer);
-        $end_time = microtime(true);
-        $log[] = "set_company_database_duration:". $to_miliseconds($end_time - $start_time);
-
         //===================================================================================
         /*
         STORE_TRANSACTION ORIGINATION
@@ -3023,18 +3012,9 @@ class ServersController extends WebServicesController
                     ) tmptbl where yread = 0
                 ) as tblecount";
         //===================================================================================
-        $start_time = microtime(true);
         $GetData = $this->Customer->query($sql);
-        $end_time = microtime(true);
-        $log[] = "query_duration:". $to_miliseconds($end_time - $start_time);
-
-        $start_time = microtime(true);
         $arr_reservation = $this->ParseDataToObjectArray($GetData, 'tblecount');
-        $end_time = microtime(true);
         //===================================================================================
-        $log[] = "ParseDataToObjectArray_function_duration:". $to_miliseconds($end_time - $start_time);
-        $arr_reservation[0]['log'] = implode("\n", $log);
-
         $ret = array();
         $ret['records'] = $arr_reservation;
         //===================================================================================
